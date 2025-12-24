b828b58a6225e3475037aec5744b8cfb0a91c313
Marcell Marton <m@heymarcell.com>
2025-12-24 02:18:35 +0100

    feat: enable promo code field in Stripe Checkout

--- src/api/routes/stripe.ts
+++ src/api/routes/stripe.ts
@@ -28,6 +28,7 @@ app.post("/checkout", async (c) => {
       "cancel_url": `${c.env.SITE_URL}/pricing?canceled=true`,
       "line_items[0][price]": priceId,
       "line_items[0][quantity]": "1",
+      "allow_promotion_codes": "true", // Enable coupon/promo code field
       "metadata[waiver_accepted]": "true", // Record that they accepted the modal on frontend
     };
 
